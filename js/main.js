29bfcf5ee65a431443e92a6efc5149c45076e2f6
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2020-10-29 15:19:03 +0200

    using localStorage

--- js/main.js
+++ js/main.js
@@ -4,23 +4,43 @@ const dom_body = document.querySelector('body');
 const dom_container = document.querySelector('.container')
 
 let start;
-let isStart = false;
+// let isStart = false;
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+// const changeColor = function(e) {
+//   if (e.target.innerText === "start" && !isStart) {
+//     isStart = true;
+//     start = setInterval(() => {
+//       randomIntegerFromInterval(0, colors.length)
+//       dom_body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length)];
+//     }, 1000);
+//   } else if (e.target.innerText === "stop") {
+//     clearInterval(start);
+//     isStart = false;
+//   }
+// }
+
 const changeColor = function(e) {
-  if (e.target.innerText === "start" && !isStart) {
-    isStart = true;
+  if (e.target.innerText === "start" && localStorage.getItem('isStart')!=='true') {
+    localStorage.setItem('isStart', 'true');
     start = setInterval(() => {
       randomIntegerFromInterval(0, colors.length)
       dom_body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length)];
     }, 1000);
   } else if (e.target.innerText === "stop") {
     clearInterval(start);
-    isStart = false;
+    localStorage.setItem('isStart', 'false');
   }
 }
+if (localStorage.getItem('isStart')==='true') {
+  start = setInterval(() => {
+    randomIntegerFromInterval(0, colors.length)
+    dom_body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length)];
+  }, 1000);
+}
+
 
 dom_container.addEventListener('click', changeColor);
 
